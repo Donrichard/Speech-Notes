d9dfca817dc0f79bcc759379d1651fdf8b073366
Richardd <richardrikimaru@gmail.com>
2018-04-02 23:48:08 +0700

    Initial Commit

---
+++ Speech Notes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Speech Notes
+//
+//  Created by Richard Richard on 02/04/18.
+//  Copyright © 2018 Richard Richard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
